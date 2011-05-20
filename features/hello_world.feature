41d0a55342c822aa30bc5235bc26ef666426ebc6
Andrew Kiellor <akiellor@gmail.com>
2011-05-20 20:28:55 +0800

    Added wildcard for things we don't care about

--- features/hello_world.feature
+++ features/hello_world.feature
@@ -1,9 +1,17 @@
 Feature: HelloWorld app says "Hello World"
 
-Scenario: Simple get on app root
+Scenario: GET /
   Given the "HelloWorld" app is running
   When an api client performs GET /
   Then the json response should look like:
   """
     {'message': 'Hello World'}
+  """
+
+Scenario: GET / has message
+  Given the "HelloWorld" app is running
+  When an api client performs GET /
+  Then the json response should look like:
+  """
+    {'message': '{{...}}'}
   """
--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -11,7 +11,17 @@ When /^an api client performs ([^\s]*) (.*?)$/ do |method, url|
   @response = RestClient.send(method.downcase.to_sym, "localhost:3000#{url}")
 end
 
-Then /^the json response should look like:$/ do |response_body|
-  ActiveSupport::JSON.decode(@response).should == ActiveSupport::JSON.decode(response_body)
+Then /^the json response should look like:$/ do |response_body_string|
+  response_body = ActiveSupport::JSON.decode(response_body_string).map do |entry|
+    [entry[0], (entry[1] == '{{...}}' ? Any.new : entry[1])]
+  end
+
+  Hash[response_body].should == ActiveSupport::JSON.decode(@response)
+end
+
+class Any
+  def == other
+    true
+  end
 end
 
