3c6ec967321228097cdb4ab64f70b3bc33bba3ac
Andrew Kiellor <akiellor@gmail.com>
2011-05-29 21:47:10 +1000

    Added support for header wildcards in http_description_parser

--- spec/contracted/http_description_parser_spec.rb
+++ spec/contracted/http_description_parser_spec.rb
@@ -1,26 +1,41 @@
 require 'polyglot'
 require 'treetop'
 require 'contracted/http_description'
-require 'pp'
+require 'contracted/http_description/matchers'
 
 describe HttpDescriptionParser do
-  let(:description_string) { "HTTP/1.1 200 OK\n\n{'message': 'hello'}" }
-
   let(:description) { HttpDescriptionParser.new.parse(description_string) }
 
-  describe "description" do
-    subject { description }
+  context "no headers as part of description" do
+    let(:description_string) { "HTTP/1.1 200 OK\n\n{'message': 'hello'}" }
+    
+    describe "description" do
+      subject { description }
 
-    its(:headers) { subject.text_value.should == "" }
-    its(:body) { subject.text_value.should == "{'message': 'hello'}" }
+      its(:headers) { subject.matcher.should be_an_instance_of Contracted::HttpDescription::SameHeadersMatcher }
+      its(:headers) { subject.values.should == [] }
+      its(:headers) { subject.text_value.should == '' }
+      its(:body) { subject.text_value.should == "{'message': 'hello'}" }
+    end
+
+    describe "status line" do
+      subject { description.status_line }
+
+      its(:status) { subject.text_value.should == "200" }
+      its(:version) { subject.text_value.should == "1.1" }
+      its(:reason) { subject.text_value.should == "OK" }
+    end
   end
 
-  describe "status line" do
-    subject { description.status_line }
+  context "wildcard header in description" do
+    let(:description_string) { "HTTP/1.1 200 OK\nContent-Type: application/json\n...\n\n{'message': 'hello'}" }
 
-    its(:status) { subject.text_value.should == "200" }
-    its(:version) { subject.text_value.should == "1.1" }
-    its(:reason) { subject.text_value.should == "OK" }
+    describe "description" do
+      subject { description }
+
+      its(:headers) { subject.values.should == [{'Content-Type' => 'application/json'}, '...'] }
+      its(:headers) { subject.matcher.should be_an_instance_of Contracted::HttpDescription::IncludeHeadersMatcher }
+    end
   end
 end
 
